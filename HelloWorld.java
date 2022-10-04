da7ffe443e8a139ff36e3696b4d62cfe9a6e84ef
inan77 <inanust@gmail.com>
2022-10-04 14:44:14 -0400

    asfsaff

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-    /*
-    some
-    codes
-    for
-    landingPage
-     */
-
-        /*
-        Some
-        codes
-        for
-        loginPage
-       */
 
     }
+
 }
