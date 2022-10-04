fcfae98a4a8f243ff8ba32dad4feac72c665f07b
inan77 <inanust@gmail.com>
2022-10-04 14:12:56 -0400

    Conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Helloworld");
-        System.out.println("Helloworld2");
-
+    /*
+    some
+    codes
+    for
+    landingPage
+     */
 
     }
 }
